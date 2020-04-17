29e3f85146886847d7e7396fdfed9114e997df97
Patricio <patricioprp06@gmail.com>
2020-04-17 00:28:38 -0300

    se incorpora la navegacion para el listado de empleados, crear empleado y el boton de agregar empleados

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Scene, Router } from "react-native-router-flux";
+import { Scene, Router, Actions } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
-import EmployeeList from "./components/EmpleoyeeList";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" initial/>
-        <Scene key="employeeList" component={EmployeeList} title="Employee List"/>
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+            initial
+          />
+        </Scene>
+        <Scene key="main">
+          <Scene
+          rightTitle="Add"
+          onRight={()=> Actions.employeeCreate()}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employee List"
+          />
+        </Scene>
+        <Scene key="employeeCreate" component={EmployeeCreate} title="Employee Create"/>
       </Scene>
     </Router>
   );
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeCreate extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Form de empleados</Text>
+            </View>
+        );
+    }
+}
+export default EmployeeCreate;
