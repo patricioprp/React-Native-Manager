a7a4c74e792ec8679bbef2457205ff440115e17d
Patricio <patricioprp06@gmail.com>
2020-04-18 23:01:49 -0300

    se conecta el formulario de creacion con los reducers y los actions

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card,CardSection,Button,Input } from './common';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Button, Input } from './common';
 
 class EmployeeCreate extends Component {
-    render(){
-        return(
-            <Card>
-                <CardSection>
-                    <Input 
-                    label="Name"
-                    placeholder="Nombre"
-                    />
-                </CardSection>
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input 
+          label="Name" 
+          placeholder="Nombre" 
+          value={this.props.name}
+          onChangeText={value => {this.props.employeeUpdate({prop: 'name', value})}}
+          //Es equivalente a onChangeText={text => {this.props.employeeUpdate({prop: 'name', value: text})}}
+          />
+        </CardSection>
 
-                <CardSection>
-                    <Input 
-                    label="Phone"
-                    placeholder="555-5555-5555"
-                    />
-                </CardSection>
+        <CardSection>
+          <Input 
+          label="Phone" 
+          placeholder="555-5555-5555" 
+          value={this.props.phone}
+          onChangeText={ value => {this.props.employeeUpdate({prop: 'phone', value})}}
+          />
+        </CardSection>
 
-                <CardSection>
-                    <Input />
-                </CardSection>
+        <CardSection>
+          <Input />
+        </CardSection>
 
-                <CardSection>
-                    <Button>
-                        Create
-                    </Button>
-                </CardSection>
-            </Card>
-        );
-    }
+        <CardSection>
+          <Button>Create</Button>
+        </CardSection>
+      </Card>
+    );
+  }
 }
-export default EmployeeCreate;
+
+const mapStateToProps = (employeeFromReducer) =>{
+    const { name, phone, shift } = employeeFromReducer;
+
+    return { name, phone, shift};
+};
+
+export default connect(mapStateToProps,{employeeUpdate})(EmployeeCreate);
